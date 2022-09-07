84707a2bb64513693168b0926235d88c853f066d
Eugênio Carvalho <eugeniofrcarvalho@gmail.com>
2022-09-07 10:22:40 -0300

    deleting items

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -7,6 +7,10 @@ class ListaNegociacoes {
     this._negociacoes.push(negociacao);
   }
 
+  esvazia() {
+    this._negociacoes = [];
+  }
+
   get negociacoes() {
     // Defense Programming: creating a copy from array this._negociacoes
     return [].concat(this._negociacoes);
