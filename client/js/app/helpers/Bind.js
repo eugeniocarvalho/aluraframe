4fe6a17a3823179ea44f91060d80a59019aae2bd
Eugênio Carvalho <eugeniofrcarvalho@gmail.com>
2022-09-12 11:12:56 -0300

    added comments

--- client/js/app/helpers/Bind.js
+++ client/js/app/helpers/Bind.js
@@ -1,6 +1,7 @@
 class Bind {
   constructor(model, view, props) {
-    // cria a proxy com os parâmetros passado por parametro
+    // cria a proxy com os parâmetros passado por parametro, modelo, propriedades e ação
+    //toda vez que essas propriedades forem acessadas ele atualiza o view passando o model
     let proxy = ProxyFactory.create(model, props, (model) => view.update(model));
 
     //antes de devolver a proxy, atualiza o modelo
